efaabece5b6d6b2319106bdd66a265a32073168f
VincentTuan <vincentjava@yahoo.com.tw>
2023-08-03 11:15:47 +0800

    Add files via upload

---
+++ javacode/Hello2.java
@@ -0,0 +1,14 @@
+public class Hello2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(5+2); // 7
+		System.out.println(5-2); // 3
+		System.out.println(5*2); // 10
+		System.out.println(5/2); // 2
+		System.out.println(5.0/2); // 2.5
+		System.out.println(5%2); // 1
+
+	}
+
+}
