8891765f6518714c2ce61e38c16e9314116462e8
VincentTuan <vincentjava@yahoo.com.tw>
2023-08-03 14:27:30 +0800

    Add files via upload

---
+++ javacode/Hello9.java
@@ -0,0 +1,12 @@
+public class Hello9 {
+
+	public static void main(String[] args) {
+		// 今有雞,兔同籠, 上有35頭, 下有94足, 問雞兔各幾何 ?
+		int total = 35;
+		int feet = 94;
+		int x = (feet - (total*2))/(4-2);
+		int y = total - x;
+		System.out.printf("兔 %d 雞 %d\n", x, y);
+	}
+
+}
