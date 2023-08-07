501e748c5a4d226f14e187a6e1aae4760a05e568
VincentTuan <vincentjava@yahoo.com.tw>
2023-08-07 09:32:29 +0800

    Add files via upload

---
+++ javacode/day3/WhileDemo2.java
@@ -0,0 +1,18 @@
+package day3;
+
+public class WhileDemo2 {
+
+	public static void main(String[] args) {
+		int count = 3;
+
+		while(count > 0) {
+			System.out.println(count + " Java");
+			count--;
+			System.out.println(count);
+			System.out.println();
+		}
+
+
+	}
+
+}
