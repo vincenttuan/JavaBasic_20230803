ee7192c1357d486fd289197bdcfd6148b7456c2a
VincentTuan <vincentjava@yahoo.com.tw>
2023-08-07 11:46:01 +0800

    Add files via upload

---
+++ javacode/day3/ForLoop2.java
@@ -0,0 +1,45 @@
+package day3;
+
+/*
+利用 for 迴圈印出
+
+*
+**
+***
+****
+*****
+
+*/
+
+public class ForLoop2 {
+
+	public static void main(String[] args) {
+		
+		for(int y=1;y<=1;y++) {
+			System.out.print("*");			
+		}
+		System.out.println();
+
+		for(int y=1;y<=2;y++) {
+			System.out.print("*");			
+		}
+		System.out.println();
+		
+		for(int y=1;y<=3;y++) {
+			System.out.print("*");			
+		}
+		System.out.println();
+
+		for(int y=1;y<=4;y++) {
+			System.out.print("*");			
+		}
+		System.out.println();
+
+		for(int y=1;y<=5;y++) {
+			System.out.print("*");			
+		}
+		System.out.println();
+
+	}
+
+}
