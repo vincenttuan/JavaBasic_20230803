f8ff85833b070e39e4f5003e640ab921202ab103
VincentTuan <vincentjava@yahoo.com.tw>
2023-08-07 11:33:25 +0800

    Add files via upload

--- javacode/day3/NineTable.java
+++ javacode/day3/NineTable.java
@@ -7,7 +7,7 @@ public class NineTable {
 		for(int x=1;x<=9;x++) {
 
 			for(int y=1;y<=9;y++) {
-				System.out.printf("%d*%d=%d ", x, y, (x*y));
+				System.out.printf("%2d*%2d=%2d ", x, y, (x*y));
 			}
 			System.out.println(); // 換行
 
